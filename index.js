950670ce5579cc296d04379158ec697fa8ed2235
Brad Christensen <brad@christensen.co.nz>
2017-07-08 13:49:51 +1200

    Implement 2-pass encoding correctly; drop support for Node 7.x

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 
+const fs = require('fs');
 const path = require('path');
+const os = require('os');
+const { promisify } = require('util');
 const parseArgs = require('minimist');
 const shelljs = require('shelljs');
 const createQueue = require('queue');
-const { spawn } = require('child_process');
+const childProcess = require('child_process');
 const cpuStat = require('cpu-stat');
+const uuid = require('uuid');
+
+const unlink = promisify(fs.unlink);
+
+const isWindows = os.platform() === 'win32';
+const devNull = isWindows ? 'NUL' : '/dev/null';
+
+function spawn(...args) {
+  return new Promise((resolve) => {
+    const process = childProcess.spawn(...args);
+
+    process.stderr.on('data', data => console.log(`${data}`));
+
+    process.on('close', () => {
+      resolve();
+    });
+  });
+}
 
 const args = parseArgs(process.argv.slice(2));
 
@@ -33,17 +54,19 @@ const files = shelljs
 
 const queue = createQueue();
 
-const createFfmpegArgs = (inputFile, outputFile) => {
-  const strArgs = '-i INPUTFILE -nostats -hide_banner -loglevel error -n -threads 1 -c:v mpeg4 ' +
-    '-vtag xvid -pass 2 -b:v 3000k -vf scale=720:-1 -c:a libmp3lame -b:a 192k OUTPUTFILE';
+const createFfmpegArgs = (inputFile, outputFile, logFileId, pass = 1) => {
+  const passDependentFlags = pass === 1 ? '-y -f avi' : '-n';
+  const strArgs = '-i INPUTFILE -nostats -hide_banner -loglevel error -threads 1 -c:v mpeg4 ' +
+    `-vtag xvid -pass ${pass} -passlogfile ${logFileId} -b:v 3000k -vf scale=720:-1 ` +
+    `${passDependentFlags} -c:a libmp3lame -b:a 192k OUTPUTFILE`;
   const newArgs = strArgs.split(' ');
   newArgs[1] = inputFile;
-  newArgs[newArgs.length - 1] = outputFile;
+  newArgs[newArgs.length - 1] = pass === 1 ? devNull : outputFile;
   return newArgs;
 };
 
 files.forEach((file) => {
-  queue.push((cb) => {
+  queue.push(async (cb) => {
     const inputFile = path.resolve(file);
     const basename = path.basename(inputFile);
     const basenameWithoutExt = path.basename(inputFile, path.extname(inputFile));
@@ -53,15 +76,28 @@ files.forEach((file) => {
       cb();
     } else {
       console.log(`Encoding file ${basename}...`);
-      const shellArgs = createFfmpegArgs(inputFile, outputFile);
-      const process = spawn('ffmpeg', shellArgs);
 
-      process.stderr.on('data', data => console.log(`${data}`));
+      const id = uuid();
+      const firstPass = createFfmpegArgs(inputFile, outputFile, id, 1);
+      const secondPass = createFfmpegArgs(inputFile, outputFile, id, 2);
 
-      process.on('close', () => {
+      try {
+        await spawn('ffmpeg', firstPass);
+        console.log(`... Completed first pass of file ${basename}`);
+
+        await spawn('ffmpeg', secondPass);
         console.log(`... Completed encoding file ${basename}`);
-        cb();
-      });
+      } catch (err) {
+        console.error(`Encode failed for file ${basename}`, err);
+      }
+
+      try {
+        await unlink(`${id}-0.log`);
+      } catch (err) {
+        console.error(`Unable to delete 2-pass log file for ${basename}`, err);
+      }
+
+      cb();
     }
   });
 });
